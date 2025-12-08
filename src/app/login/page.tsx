2e2984271290693d399ee55fa084cd894703cd11
Sabien <ambid17@gmail.com>
2025-12-07 22:35:34 -0600

    Fix versioning and formgroup

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,23 @@
 import { Button, Form, FormControl, FormGroup, FormLabel, FormText } from "react-bootstrap";
+//import { Form.Group } from "react-bootstrap";
 export default function login() {
     return (
         <div>
             <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
                 <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
                     <Form>
-                        <FormGroup className="mb-3" controlId="formBasicEmail">
+                        <div className="mb-3">
                             <FormLabel>Email address</FormLabel>
                             <FormControl type="email" placeholder="Enter email" />
                             <FormText className="text-muted">
                                 We'll never share your email with anyone else
                             </FormText>
-                        </FormGroup>
+                        </div>
 
-                        <FormGroup className="mb-3" controlId="formBasicPassword">
+                        <div className="mb-3" >
                             <FormLabel>Password</FormLabel>
                             <FormControl type="password" placeholder="Password" />
-                        </FormGroup>
+                        </div>
                         <Button variant="primary" type="submit">
                             Submit
                         </Button>
